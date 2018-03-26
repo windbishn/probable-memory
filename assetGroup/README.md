f196ce4d564c3a61bfbd9981efbe0d0841f5a767
Windbish <NPW0EE@multihosp.net>
2018-03-26 16:29:48 -0400

    Updates

---
+++ assetGroup/README.md
@@ -0,0 +1,7 @@
+# assetgroupList
+
+# assetPurge
+
+# assetgroupCleanup
+
+# assetgroupHostList
---
+++ assetGroup/assetPurge.py
@@ -0,0 +1,25 @@
+import qualysapi, sys
+from lxml import objectify
+
+# Connect to QualysGuard.
+qgc = qualysapi.connect()
+
+# Capture number of days from command parameter.
+number_of_days = sys.argv[1]
+
+# Request assets older than number_of_days.
+assets = qgc.request('asset_search.php', {'last_scan': 'not_within:%s' % number_of_days, 'target_asset_groups': 'All'})
+root = objectify.fromstring(assets)
+
+# Parse assets XML for list of IPs.
+
+ips = ''
+for host in root.HOST_LIST.HOST:
+    for ip in host.IP:
+        ips += ip.text + ','
+ips = ips[:-1]
+
+# Purge assets older than number_of_days.
+print ips
+purge = qgc.request('/api/2.0/fo/asset/host/', {'action': 'purge', 'ips': ips})
+print purge
