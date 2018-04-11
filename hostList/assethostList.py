23ce34fcc7472ed4e2c3cce1066ebf30c4434804
NPW0EE-A <npw0ee-a@adcvutlseclx001.multihosp.net>
2018-04-11 10:04:24 -0400

    New file

--- hostList/assethostList.py
+++ hostList/assethostList.py
@@ -6,41 +6,41 @@ import re
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
-# Setup connection to QualysGuard API.
-qgc = qualysapi.connect('../../config.qcrc')
 
+with open('ahs_subnets.csv', 'r') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		#defining variable to be used in Qualys API call
+		subnetRange = (row[0])
 
-# API v2 Host List: list hosts in [] asset group(s), comma separated. 
-call = '/api/2.0/fo/asset/host/'
-parameters= {'action': 'list', 'ag_ids': '217806', 'details': 'Basic'}
+		# Setup connection to QualysGuard API.
+		qgc = qualysapi.connect('../../config.qcrc')
 
-xml_output = qgc.request(call, parameters)
+		# API v2 Host List: list host alreay scanned. 
+		call = '/api/2.0/fo/asset/host/'
+		parameters= {'action': 'list', 'ips': subnetRange, 'details': 'Basic'}
+
+		xml_output = qgc.request(call, parameters)
 	
-root = ET.fromstring(xml_output)
+		root = ET.fromstring(xml_output)
 
-timestamp = datetime.now().strftime("%Y%m%d.%H%M%S")
-	    		
-		# print xml_output
-				
-		# print root.tag
-		# print root[0].text
-		# print root.attrib
+		timestamp = datetime.now().strftime("%Y%m%d.%H%M%S")
 
-# Open file for writing
-with open('assetgrouphostList_'+ timestamp + '.csv', 'ab') as csvfile:
+		with open ('subnet_Status.csv', 'a') as csvfile:
 				writer = csv.writer(csvfile)
-				row = ['IP', 'DNS']
+				row = ['subnetRange', 'status']
 				writer.writerow(row)
-				for host in root.iter('HOST'):
-				
-					hostIP = host.find('IP').text
-					findDNS = host.find('DNS')
-					
-					if findDNS is None:
-						DNS = "noDNS"
+
+			#Check if subnet has been scanned.
+				for host in root.iter('RESPONSE'):
+					subnetScanned = host.find('HOST_LIST')
+		
+					if subnetScanned is None:
+						status = "notScanned"
+					# print subnetRange, status
 					else:
-						DNS = host.find('DNS').text
-						
-					row = [hostIP, DNS]
-					
-					writer.writerow(row)		
+						status = "scanned"
+					# print subnetRange, status
+
+					row = [subnetRange, status ]
+					writer.writerow(row)
